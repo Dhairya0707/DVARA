5b9a6f59aa62bb462c20480db2cfe834279bae6a
Dhairya0”707git config --global user.email “dhairyadarji027@gmail.com <dhairyadarji027@gmail.com>
2026-05-01 15:56:45 +0530

    feat: add connection timeout, keep-alive, and IPv4 settings to Redis configuration

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -9,8 +9,15 @@ dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// const redis = new Redis(process.env.REDIS_URL, {
+//   maxRetriesPerRequest: null,
+// });
+
 const redis = new Redis(process.env.REDIS_URL, {
   maxRetriesPerRequest: null,
+  connectTimeout: 10000, // Wait 10s for connection
+  keepAlive: 1000, // Keep the connection alive
+  family: 4, // Use IPv4 to avoid potential DNS issues
 });
 
 export const luaScript = fs.readFileSync(
